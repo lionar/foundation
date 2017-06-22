7974898b14bceb2d6479a2beec518888316d5124
Aron Wouters <lionar@mail.com>
2017-06-22 15:12:58 +0200

    Now loading in project business logic.

--- start.php
+++ start.php
@@ -7,7 +7,29 @@ $app->share ( 'app', function ( ) use ( $app ) { return $app; } );
 
 facade::setFacadeApplication ( $app );
 
-$app->share ( 'path', function ( )
+
+/*
+|--------------------------------------------------------------------------
+| Path
+|--------------------------------------------------------------------------
+| Set up path to the root of the application.
+| 
+|
+*/
+
+$path = new path\collection ( '../..' );
+
+$app->share ( 'path', function ( ) use ( $path )
 {
-	return new path\collection ( '../..' );
+	return $path;
 } );
+
+/*
+|--------------------------------------------------------------------------
+| Application
+|--------------------------------------------------------------------------
+| Load in the application business rules.
+| 
+|
+*/
+filesystem\require_recursive ( $path->to ( 'application' ) );
